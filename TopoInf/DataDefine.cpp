d5a15efab6a8f57a0a51add6c6b9e1f1a0e7a8de
jiaoyanshi@windows <ludaoquan@163.com>
2016-03-15 10:52:30 +0800

    add ctrollermsg server

--- TopoInf/DataDefine.cpp
+++ TopoInf/DataDefine.cpp
@@ -1,1 +1,20 @@
+#include "SDNDataDefine.h"
 
+SockData::SockData()
+{
+	msgType = 1;
+}
+
+SockData::SockData(const SockData &_data)
+{
+	if ( &_data == this)
+	{
+		return;
+	}
+	msgType = _data.msgType;
+}
+
+SockData::~SockData()
+{
+
+}
